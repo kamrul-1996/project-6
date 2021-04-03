adcafd2c54d1a86f1d3864c143493344376f176b
kamrul-1996 <kamrul.islam.kth.1996@gmail.com>
2021-04-03 20:13:08 +0600

    new file

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,14 @@
 $(function () {
 	//varable declare;
 	var backtop = $('.backtoTop');
-	
+
 	//navbar active on mouseover;
 
 	$('.navbar .nav-item').on('mouseover', function () {
 		$(this).addClass('active').siblings().removeClass('active');
 	});
 
-	
+
 	// fixed navbar;
 	$(window).scroll(function () {
 		var navscroll = $(this).scrollTop();
@@ -27,8 +27,8 @@ $(function () {
 			backtop.fadeOut();
 		}
 	});
-	
-	
+
+
 	// backtoTop body-0 ;
 	backtop.click(function () {
 		$('html,body').animate({
@@ -45,7 +45,7 @@ $(function () {
 		autoplaySpeed: 3000,
 	});
 
-	
+
 	// about video;
 	$('.venobox').venobox({
 		spinner: 'cube-grid',
@@ -54,7 +54,7 @@ $(function () {
 		overlayColor: 'var(--over8)',
 	});
 
-	
+
 	// testimonial slider;
 	$('.test-slid').slick({
 		slidesToShow: 2,
@@ -62,9 +62,27 @@ $(function () {
 		arrows: false,
 		autoplay: true,
 		autoplaySpeed: 1500,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: false,
+				}
+    		},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+    },
+		]
 	});
 
-	
+
 	// counter up;
 	$('.counter').counterUp({
 		time: 1500,
@@ -78,32 +96,59 @@ $(function () {
 		slidesToScroll: 1,
 		arrows: true,
 		autoplay: true,
-		//		autoplaySpeed:1500,
 		prevArrow: '<i class="slick-prev fas fa-chevron-left"></i>',
 		nextArrow: '<i class="slick-next fas fa-chevron-right"></i>',
 		centerMode: true,
 		centerPadding: '0',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: false,
+				}
+    		},
+			
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					prevArrow: false,
+					nextArrow:false,
+				}
+			},
+		]
 	});
 
-	
+
 	// animate__animated;
 
 	new WOW().init();
 
- //animation body scroll ;
-    var html_body = $('html, body');
-    $('.navbar .navbar-nav .nav-item a').on('click', function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                html_body.animate({
-                    scrollTop: target.offset().top - 0
-                }, 1000);
-                return false;
-            }
-        }
-    });
+	//animation body scroll ;
+	var html_body = $('html, body');
+	$('.navbar .navbar-nav .nav-item a').on('click', function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				html_body.animate({
+					scrollTop: target.offset().top - 0
+				}, 1000);
+				return false;
+			}
+		}
+	});
 
 
 
